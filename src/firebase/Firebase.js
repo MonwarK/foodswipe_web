0fa58ce0eb8c0e23b52eff205ca22670a33d934c
Monwar Khan <72820069+MonwarK@users.noreply.github.com>
2021-06-24 14:08:32 +0100

    Update Firebase.js

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIQNESLkfPDwKyLYOaWDt1iAUDqp3jlpY",
-    authDomain: "foodswipe-d2297.firebaseapp.com",
-    databaseURL: "https://foodswipe-d2297.firebaseio.com",
-    projectId: "foodswipe-d2297",
-    storageBucket: "foodswipe-d2297.appspot.com",
-    messagingSenderId: "211953710318",
-    appId: "1:211953710318:web:01725192c8047388706fff",
-    measurementId: "G-EFLCYW89W4"
+   
 };
 
 firebase.initializeApp(firebaseConfig)
